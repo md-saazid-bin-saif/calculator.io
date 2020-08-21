fb3f0e6b7c73933307ef3a5afa5b578ce81adfa4
md-saazid-bin-saif <66367907+md-saazid-bin-saif@users.noreply.github.com>
2020-08-21 18:20:06 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+var resultField = $("#result");
+function insertNumber(number){
+    var existingNumbers = resultField.val();
+    resultField.val(existingNumbers +number)
+}
+
+function clearResult(){
+    resultField.val('');
+}
+
+function calculate(){
+    var result = eval($("#result").val())
+    resultField.val(result)
+}
+
+function deleteNumber(){
+   var presentValue = resultField.val();
+   if(presentValue!=''){
+    resultField.val(presentValue.slice(0,-1));   
+   }
+}
